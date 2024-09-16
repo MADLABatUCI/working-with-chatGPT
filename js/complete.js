c1a9ef5e60de500ba47a12356320a2a601937ff7
Helio Tejeda <htejeda@uci.edu>
2024-09-16 15:40:47 -0700

    Updated completion redirect url

--- js/complete.js
+++ js/complete.js
@@ -68,7 +68,9 @@ $(document).ready(function (){
         //  Redirect URL for Open Ended Experiment 01 (new) 10 Sep. 2024
         //window.location.replace("https://app.prolific.com/submissions/complete?cc=C1031R79");
         //  Redirect URL for Open Ended Experiment 01 (new) 12 Sep. 2024 (1 exp style)
-        window.location.replace("https://app.prolific.com/submissions/complete?cc=CS7WXECA");
+        //window.location.replace("https://app.prolific.com/submissions/complete?cc=CS7WXECA");
+        //  Redirect URL for Open Ended Experiment 01 (new) 12 Sep. 2024 (1 exp style)
+        window.location.replace("https://app.prolific.com/submissions/complete?cc=CCKXO7T3");
     }
 
     function submitFeedback() {
