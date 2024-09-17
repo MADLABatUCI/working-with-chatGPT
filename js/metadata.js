cd93e147874484b080f9e42bf0f415dca2f4c916
Helio Tejeda <htejeda@uci.edu>
2024-09-17 13:20:34 -0700

    Updated firebase db key for experiment name

--- js/metadata.js
+++ js/metadata.js
@@ -23,7 +23,7 @@ var DEBUG = false;
 //      Experiment Name
 //var EXPERIMENT_NAME             = "Calibrating User Confidence in GPT";
 var EXPERIMENT_NAME             = "Working with ChatGPT"; //"Estimating Confidence : Question Answer Systems";
-var EXPERIMENT_DATABASE_NAME    = "working-with-chatgpt-openended-experiment-vA";
+var EXPERIMENT_DATABASE_NAME    = "working-with-chatgpt-openended-experiment-vB";
 
 //      Section Headers
 var SECTION_TITLE_CONSENT_PAGE  = "Consent Page";
