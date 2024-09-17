67efb350b9fb9db25686ad7ebcffaf688a1a63c9
Helio Tejeda <htejeda@uci.edu>
2024-09-17 13:19:59 -0700

    Updated json file to use for questions and this version is now using 9 explanation styles

--- js/experiment.js
+++ js/experiment.js
@@ -36,8 +36,8 @@ import {
 //      Task Information
 var CURRENT_TASK                = 1;
 var TOTAL_TRIALS                = 40;
-var EXPLANATION_OPTIONS         = 1;
-var DATA_FILE                   = "data/openquestions_336_explanationstyle01_v02.json";
+var EXPLANATION_OPTIONS         = 9;
+var DATA_FILE                   = "data/openquestions_336_explanationstyle09_v02.json";
 
 //      Database Path
 var TRIAL_DB_PATH               = EXPERIMENT_DATABASE_NAME + '/participantData/' + firebaseUserId + '/trialData';
